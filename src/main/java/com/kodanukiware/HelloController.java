22b9486b7d23b6df48603be44364839190412ae6
TakayukiIsano <83534651+TakayukiIsano@users.noreply.github.com>
2022-12-15 13:38:06 +0900

    comitted

--- src/main/java/com/kodanukiware/HelloController.java
+++ src/main/java/com/kodanukiware/HelloController.java
@@ -4,7 +4,6 @@ import ajd4jp.AJD;
 import ajd4jp.Day;
 import ajd4jp.STCD;
 import ajd4jp.SexagenaryCycle;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
